fcca96a1bcac1dbab6debd6ab1e973fc597ebec8
Eduardo Pinho <enet4mikeenet@gmail.com>
2015-01-07 15:12:34 +0000

    New credits page w/ contributors ; changed footer copyright notice in main menu

---
+++ credits.h
@@ -0,0 +1,14 @@
+/* Macro definitions used by the credits screen.
+ * (this may hopefully be automatically generated)
+ */
+
+#define GAME_VERSION "0.1"
+
+#define GAME_ORIGINAL_VERSION "1.3.4.1"
+
+#define CONTRIBUTORS \
+	" Vincent Verhoevenv \\" \
+        " Eduardo Pinho "
+
+#define COPYRIGHT "Copyright (c) 2008 Vincent Verhoevenv -" \
+	" Portions Copyright (c) 2003 Ville Mönkkönen "
